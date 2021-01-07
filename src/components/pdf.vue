9dd36846c22103a4c08eda00e6ce71640ee9f93e
刘梓洋 <liuziyang@liuziyangdeMacBook-Pro.local>
2021-01-08 00:17:26 +0800

    更改为require方式引入pdfjs-dist

--- src/components/pdf.vue
+++ src/components/pdf.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <canvas v-for="page in pdfList" :id="'the-canvas'+page" :key="page"></canvas>
+    <canvas
+      v-for="page in pdfList"
+      :id="'the-canvas' + page"
+      :key="page"
+    ></canvas>
   </div>
 </template>
 <script>
 import PDFJS from "pdfjs-dist";
-import pdfjsLib from "pdfjs-dist";
+const pdfjsLib = require("pdfjs-dist");
 // pdf二进制base64数据
-import pdfdata from './data'
+import pdfdata from "./data";
 export default {
   data() {
     return {
       pdfDoc: null,
-      pdfList: 0
+      pdfList: 0,
     };
   },
   mounted() {
@@ -24,10 +28,10 @@ export default {
   methods: {
     // 初始化pdfjs
     initThePDFJSLIB() {
-      pdfjsLib.GlobalWorkerOptions.workerSrc = "pdfjs-dist/build/pdf.worker.js";
+      pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.js");
     },
     _renderPage(num) {
-      this.pdfDoc.getPage(num).then(page => {
+      this.pdfDoc.getPage(num).then((page) => {
         let canvas = document.getElementById("the-canvas" + num);
         let ctx = canvas.getContext("2d");
         let dpr = window.devicePixelRatio || 1;
@@ -49,7 +53,7 @@ export default {
         ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
         let renderContext = {
           canvasContext: ctx,
-          viewport: viewport
+          viewport: viewport,
         };
         page.render(renderContext);
         if (this.pdfList > num) {
@@ -58,27 +62,28 @@ export default {
       });
     },
     loadFile(base64Str) {
-      window.console.log(base64Str,"base64Str");
-      
-      PDFJS.getDocument({
-        // atob将base64转成流文件
-        ///data对象可以换成url
-        data: window.atob(base64Str),
-        cMapUrl: "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.943/cmaps/",
-        cMapPacked: true
-      }).then(pdf => {
-        this.pdfDoc = pdf;
-        this.pdfList = this.pdfDoc.numPages;
-        this.$nextTick(() => {
-          this._renderPage(1);
+      window.console.log(base64Str, "base64Str");
+      pdfjsLib
+        .getDocument({
+          // atob将base64转成流文件
+          ///data对象可以换成url
+          data: window.atob(base64Str),
+          // cMapUrl: "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.943/cmaps/",
+          cMapPacked: true,
+        })
+        .then((pdf) => {
+          this.pdfDoc = pdf;
+          this.pdfList = this.pdfDoc.numPages;
+          this.$nextTick(() => {
+            this._renderPage(1);
+          });
         });
-      });
-    }
+    },
   },
-  components: {}
+  components: {},
 };
 </script>
-<style lang="scss" scoped>
+<style>
 canvas {
   display: block;
   border-bottom: 1px solid black;
